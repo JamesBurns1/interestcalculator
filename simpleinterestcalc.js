721b097b0b002d901c8ef36bf65a8d8c8ab83413
James Burns <74157348+JamesBurns1@users.noreply.github.com>
2023-06-21 09:13:59 -0700

    Add files via upload
    original upload

---
+++ simpleinterestcalc.js
@@ -0,0 +1,39 @@
+function compute()
+{
+    //variables for the operation
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value
+    var interest = principal * years * rate /100;
+    
+    var year = new Date().getFullYear()+parseInt(years);
+
+
+    //if else statement to validate operation
+    if(principal ==undefined || principal <=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    else{
+        var inr = "If you deposit <mark>" + "$" + principal+"</mark><br>";
+        var ra = "at an interest rate of <mark>" + rate+"%</mark><br>";
+        var am = "You will receive an amount of <mark>"+ "$" +interest+"</mark><br>";
+        var ye = "in the year <mark'>" + year +"</mark>"; 
+        
+        document.getElementById("result").innerHTML=inr+ra+am+ye;
+
+        
+    }
+
+}
+
+function updateRate(){
+
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
+    
+
+}
+
