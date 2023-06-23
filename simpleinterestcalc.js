d69082aa19fe4edf9aba38f2fbaeb863235191e2
James Burns <74157348+JamesBurns1@users.noreply.github.com>
2023-06-23 16:49:50 -0700

    Update simpleinterestcalc.js
    Adding toFixed(2) to interest for cleaner result

--- simpleinterestcalc.js
+++ simpleinterestcalc.js
@@ -18,7 +18,7 @@ function compute()
     else{
         var inr = "If you deposit <mark>" + "$" + principal+"</mark><br>";
         var ra = "at an interest rate of <mark>" + rate+"%</mark><br>";
-        var am = "You will receive an amount of <mark>"+ "$" +interest+"</mark><br>";
+        var am = "You will receive an amount of <mark>"+ "$" +interest.toFixed(2)+"</mark><br>";
         var ye = "in the year <mark'>" + year +"</mark>"; 
         
         document.getElementById("result").innerHTML=inr+ra+am+ye;
